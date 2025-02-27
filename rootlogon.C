87d2a1a2bb486306f8edccd4a54e3202f8ef6b01
Hao Huang <hhuang@ifarm2401.jlab.org>
2025-02-27 10:23:12 -0500

    added the package to github (this the version from Hosan in his JLab /work directory)

---
+++ rootlogon.C
@@ -0,0 +1,18 @@
+{
+ 
+ 
+//gSystem.Load("/afs/in2p3.fr/home/throng/clas/carlos/theory/LIB/libmydvcs.so");
+
+ gSystem->Load("libTGenGeo.so");
+ gSystem->Load("libTGenBase.so");
+ gSystem->Load("libTGenDVCS.so");
+ gSystem->Load("libTGVKelly.so");
+ gSystem->Load("libTGenDeltPi0.so");
+ gSystem->Load("libTGenElas.so");
+ gSystem->Load("libTGenOme.so");
+ gSystem->Load("libTGenPi0.so");
+ gSystem->Load("libTGenPip.so");
+ gSystem->Load("libTGenRho.so");
+
+ gROOT->SetStyle("Plain");
+}
